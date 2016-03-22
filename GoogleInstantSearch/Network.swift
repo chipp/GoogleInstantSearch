5073c5ce8598f385e68c8cf0446f0fdc5fc17112
Vladimir Burdukov <chippcheg@gmail.com>
2016-03-22 22:51:07 +0300

    Implement search suggestions table with static search term

--- GoogleInstantSearch/Network.swift
+++ GoogleInstantSearch/Network.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class Network {
     
     static let instance = Network()
     
+    enum Error: ErrorType {
+        case InvalidURL
+        case InvalidResponse
+    }
+    
 //    http://google.com/complete/search?output=firefox&q=keyword
     func suggestKeywork(keyword: String) -> Observable<[String]> {
-        return Observable.empty()
+        return Observable.deferred { () -> Observable<[String]> in
+            guard let url = NSURL(string: "http://google.com/complete/search?output=firefox&q=\(keyword)") else {
+                return Observable.error(Error.InvalidURL)
+            }
+            
+            return NSURLSession.sharedSession().rx_JSON(url).map { json in
+                guard let array = json as? [AnyObject] where array.count > 1,
+                let suggest = array[1] as? [String] else {
+                    return []
+                }
+                
+                return suggest
+            }
+        }
     }
 
 }
--- GoogleInstantSearch/ViewController.swift
+++ GoogleInstantSearch/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    @IBOutlet private weak var keywordTextField: UITextField!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private let suggestions = Variable<[String]>([])
+    private let searchTerm = Variable<String?>(nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchTerm.asObservable().flatMapLatest { keyword -> Observable<[String]> in
+            guard let keyword = keyword else {
+                return Observable.just([])
+            }
+            
+            return Network.instance.suggestKeywork(keyword)
+        }.bindTo(suggestions).addDisposableTo(disposeBag)
+        
+        suggestions.asDriver()
+            .drive(tableView.rx_itemsWithCellIdentifier("SuggestCell", cellType: UITableViewCell.self)) { [weak self] (row, item, cell) in
+                cell.textLabel?.attributedText = self?.highlightSearchTerm(self?.searchTerm.value, inSuggestion: item)
+            }.addDisposableTo(disposeBag)
+        
+        searchTerm.value = "Engl"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func highlightSearchTerm(searchTerm: String?, inSuggestion suggestion: String) -> NSAttributedString? {
+        guard let searchTerm = searchTerm else {
+            return nil
+        }
+        
+        let searchTermRange = (suggestion as NSString).rangeOfString(searchTerm, options: [.CaseInsensitiveSearch])
+        let attributedString = NSMutableAttributedString(string: suggestion)
+        
+        if searchTermRange.length > 0 {
+            attributedString.addAttributes([NSForegroundColorAttributeName : UIColor.grayColor()], range: searchTermRange)
+        }
+        
+        return NSAttributedString(attributedString: attributedString)
     }
-
 
 }
 
