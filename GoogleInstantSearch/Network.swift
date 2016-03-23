a01641580c68ee9a1af8f379a88fc08fc31a9054
Vladimir Burdukov <v.burdukov@superjob.ru>
2016-03-23 12:47:44 +0300

    Add handling for empty keyword, empty network response, error cases. Add code comments.

--- GoogleInstantSearch/Network.swift
+++ GoogleInstantSearch/Network.swift
@@ -19,8 +19,8 @@ class Network {
         case InvalidResponse
     }
     
-//    http://google.com/complete/search?output=firefox&q=keyword
     func suggestKeywork(keyword: String) -> Observable<[String]> {
+        // Create "cold" observable – request will be performed only when someone subscribes it
         return Observable.deferred { () -> Observable<[String]> in
             guard let baseURL = NSURL(string: "http://google.com/complete/search"),
                 let url = self.url(baseURL, withParams: ["output" : "firefox", "q" : keyword]) else {
@@ -28,6 +28,8 @@ class Network {
             }
             
             return NSURLSession.sharedSession().rx_JSON(url).map { json in
+                // api returns json in following form: ["keyword", ["suggest1", "suggest2"]]
+                // we need only second part of this array
                 guard let array = json as? [AnyObject] where array.count > 1,
                 let suggest = array[1] as? [String] else {
                     return []
@@ -39,6 +41,8 @@ class Network {
     }
     
     private func url(url: NSURL, withParams params: [String: String]) -> NSURL? {
+        // add params with help of NSURLComponents
+        // it will help us to perform all of percent escaping operations
         let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
         components?.queryItems = params.map(NSURLQueryItem.init)
         return components?.URL
--- GoogleInstantSearch/ViewController.swift
+++ GoogleInstantSearch/ViewController.swift
@@ -8,28 +8,49 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
-    @IBOutlet private weak var keywordTextField: UITextField!
+    @IBOutlet private weak var keywordTextField: UITextField! {
+        didSet {
+            // Add left content padding for textField
+            let padding = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 8, height: keywordTextField.frame.height)))
+            keywordTextField.leftView = padding
+            keywordTextField.leftViewMode = .Always
+        }
+    }
     @IBOutlet private weak var tableView: UITableView!
-    
-    private let suggestions = Variable<[String]>([])
-    private let searchTerm = Variable<String?>(nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         keywordTextField.rx_text.asDriver()
+            // .throttle(0.5) – will wait 0.5s for new value from textField, if no value received – send latest value to subscriber
+            // .distinctUntilChanged() – will forward value only if it changed in compare to previous value from textField
             .throttle(0.5).distinctUntilChanged()
-            .flatMapLatest { keyword in
-                Observable.zip(Observable.just(keyword), Network.instance.suggestKeywork(keyword)) { keyword, suggestions in
-                    suggestions.map { [weak self] suggest in
+            .flatMapLatest { keyword -> Driver<[NSAttributedString?]> in
+                // if textField is empty then just clear tableView
+                guard keyword.characters.count > 0 else {
+                    return Observable.just([]).asDriver(onErrorJustReturn: [])
+                }
+                
+                // in other case request network for list of suggestions
+                return Observable.zip(Observable.just(keyword), Network.instance.suggestKeywork(keyword)) { keyword, suggestions -> [NSAttributedString?] in
+                    // if no suggestions found then return NSAttributedString with "No results" message
+                    guard suggestions.count > 0 else {
+                        return [NSAttributedString(string: "No results")]
+                    }
+                    // then transform list of strings to list of NSAttributedString with keyword part highlighted
+                    return suggestions.map { [weak self] suggest in
                         self?.highlightSearchTerm(keyword, inSuggestion: suggest)
                     }
-                    }.asDriver(onErrorJustReturn: [NSAttributedString(string: "❌ No results")])
-            }.drive(tableView.rx_itemsWithCellIdentifier("SuggestCell", cellType: UITableViewCell.self)) { (row, suggest, cell) in
+                    // in case of error send to subcriber NSAttributedString with message
+                }.asDriver(onErrorJustReturn: [NSAttributedString(string: "⚠️ Undefined error")])
+            }
+            // bind NSAttributedStrings array to table view as data source
+            .drive(tableView.rx_itemsWithCellIdentifier("SuggestCell", cellType: UITableViewCell.self)) { (row, suggest, cell) in
                 cell.textLabel?.attributedText = suggest
             }.addDisposableTo(disposeBag)
     }
