ba32c1c5e5fc44283d9e1f51c0045b8ae574892d
Vladimir Burdukov <chippcheg@gmail.com>
2016-10-06 15:26:51 +0300

    Added swiftlint.yml

--- GoogleInstantSearch/Network.swift
+++ GoogleInstantSearch/Network.swift
@@ -11,41 +11,41 @@ import RxSwift
 import RxCocoa
 
 class Network {
-    
-    static let instance = Network()
-    
-    enum Error: ErrorType {
-        case InvalidURL
-        case InvalidResponse
-    }
-    
-    func suggestKeyword(keyword: String) -> Observable<[String]> {
-        // Create "cold" observable – request will be performed only when someone subscribes it
-        return Observable.deferred { () -> Observable<[String]> in
-            guard let baseURL = NSURL(string: "http://google.com/complete/search"),
-                let url = self.url(baseURL, withParams: ["output" : "firefox", "q" : keyword]) else {
-                return Observable.error(Error.InvalidURL)
-            }
-            
-            return NSURLSession.sharedSession().rx_JSON(url).map { json in
-                // api returns json in following form: ["keyword", ["suggest1", "suggest2"]]
-                // we need only second part of this array
-                guard let array = json as? [AnyObject] where array.count > 1,
-                let suggest = array[1] as? [String] else {
-                    return []
-                }
-                
-                return suggest
-            }
+
+  static let instance = Network()
+
+  enum Error: ErrorType {
+    case InvalidURL
+    case InvalidResponse
+  }
+
+  func suggestKeyword(keyword: String) -> Observable<[String]> {
+    // Create "cold" observable – request will be performed only when someone subscribes it
+    return Observable.deferred { () -> Observable<[String]> in
+      guard let baseURL = NSURL(string: "http://google.com/complete/search"),
+        let url = self.url(baseURL, withParams: ["output" : "firefox", "q" : keyword]) else {
+          return Observable.error(Error.InvalidURL)
+      }
+
+      return NSURLSession.sharedSession().rx_JSON(url).map { json in
+        // api returns json in following form: ["keyword", ["suggest1", "suggest2"]]
+        // we need only second part of this array
+        guard let array = json as? [AnyObject] where array.count > 1,
+          let suggest = array[1] as? [String] else {
+            return []
         }
+
+        return suggest
+      }
     }
-    
-    private func url(url: NSURL, withParams params: [String: String]) -> NSURL? {
-        // add params with help of NSURLComponents
-        // it will help us to perform all of percent escaping operations
-        let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
-        components?.queryItems = params.map(NSURLQueryItem.init)
-        return components?.URL
-    }
+  }
+
+  private func url(url: NSURL, withParams params: [String: String]) -> NSURL? {
+    // add params with help of NSURLComponents
+    // it will help us to perform all of percent escaping operations
+    let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
+    components?.queryItems = params.map(NSURLQueryItem.init)
+    return components?.URL
+  }
 
 }
--- GoogleInstantSearch/ViewController.swift
+++ GoogleInstantSearch/ViewController.swift
@@ -11,75 +11,80 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-    
-    private let disposeBag = DisposeBag()
-    private let activityIndicator = ActivityIndicator()
-    @IBOutlet private weak var keywordTextField: UITextField! {
-        didSet {
-            // Add left content padding for textField
-            let padding = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 8, height: keywordTextField.frame.height)))
-            keywordTextField.leftView = padding
-            keywordTextField.leftViewMode = .Always
-        }
-    }
-    @IBOutlet private weak var tableView: UITableView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // bind activity indicator with UIApplications' network indicator
-        activityIndicator.drive(UIApplication.sharedApplication().rx_networkActivityIndicatorVisible).addDisposableTo(disposeBag)
-        
-        keywordTextField.rx_text.asDriver()
-            // .throttle(0.5) – will wait 0.5s for new value from textField, if no value received – send latest value to subscriber
-            // .distinctUntilChanged() – will forward value only if it changed in compare to previous value from textField
-            .throttle(0.5).distinctUntilChanged()
-            .flatMapLatest { [weak self] keyword -> Driver<[NSAttributedString?]> in
-                // if textField is empty then just clear tableView
-                guard keyword.characters.count > 0 else {
-                    return Observable.just([]).asDriver(onErrorJustReturn: [])
-                }
-                
-                // create network request
-                var networkRequest: Observable<[String]> = Network.instance.suggestKeyword(keyword)
-                // if activity indicator exists then connect request with activity indicator
-                if let activityIndicator = self?.activityIndicator {
-                    networkRequest = networkRequest.trackActivity(activityIndicator)
-                }
-                
-                // in other case request network for list of suggestions
-                return Observable.zip(Observable.just(keyword), networkRequest) { keyword, suggestions -> [NSAttributedString?] in
-                    // if no suggestions found then return NSAttributedString with "No results" message
-                    guard suggestions.count > 0 else {
-                        return [NSAttributedString(string: "No results")]
-                    }
-                    // then transform list of strings to list of NSAttributedString with keyword part highlighted
-                    return suggestions.map { [weak self] suggest in
-                        self?.highlightSearchTerm(keyword, inSuggestion: suggest)
-                    }
-                    // in case of error send to subcriber NSAttributedString with message
-                }.asDriver(onErrorJustReturn: [NSAttributedString(string: "⚠️ Undefined error")])
-            }
-            // bind NSAttributedStrings array to table view as data source
-            .drive(tableView.rx_itemsWithCellIdentifier("SuggestCell", cellType: UITableViewCell.self)) { (row, suggest, cell) in
-                cell.textLabel?.attributedText = suggest
+  private let disposeBag = DisposeBag()
+  private let activityIndicator = ActivityIndicator()
+  @IBOutlet private weak var keywordTextField: UITextField! {
+    didSet {
+      // Add left content padding for textField
+      let padding = UIView(frame: CGRect(origin: CGPoint.zero,
+        size: CGSize(width: 8, height: keywordTextField.frame.height)))
+      keywordTextField.leftView = padding
+      keywordTextField.leftViewMode = .Always
+    }
+  }
+  @IBOutlet private weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // bind activity indicator with UIApplications' network indicator
+    activityIndicator.drive(
+      UIApplication.sharedApplication().rx_networkActivityIndicatorVisible)
+      .addDisposableTo(disposeBag)
+
+    keywordTextField.rx_text.asDriver()
+      // .throttle(0.5) – will wait 0.5s for new value from textField,
+      // if no value received – send latest value to subscriber
+      // .distinctUntilChanged() – will forward value only if it changed in compare to previous value from textField
+      .throttle(0.5).distinctUntilChanged()
+      .flatMapLatest { [weak self] keyword -> Driver<[NSAttributedString?]> in
+        // if textField is empty then just clear tableView
+        guard keyword.characters.count > 0 else {
+          return Observable.just([]).asDriver(onErrorJustReturn: [])
+        }
+
+        // create network request
+        var networkRequest: Observable<[String]> = Network.instance.suggestKeyword(keyword)
+        // if activity indicator exists then connect request with activity indicator
+        if let activityIndicator = self?.activityIndicator {
+          networkRequest = networkRequest.trackActivity(activityIndicator)
+        }
+
+        // in other case request network for list of suggestions
+        return Observable.zip(Observable.just(keyword),
+          networkRequest) { keyword, suggestions -> [NSAttributedString?] in
+          // if no suggestions found then return NSAttributedString with "No results" message
+          guard suggestions.count > 0 else {
+            return [NSAttributedString(string: "No results")]
+          }
+          // then transform list of strings to list of NSAttributedString with keyword part highlighted
+          return suggestions.map { [weak self] suggest in
+            self?.highlightSearchTerm(keyword, inSuggestion: suggest)
+          }
+          // in case of error send to subcriber NSAttributedString with message
+          }.asDriver(onErrorJustReturn: [NSAttributedString(string: "⚠️ Undefined error")])
+      }
+      // bind NSAttributedStrings array to table view as data source
+      .drive(tableView.rx_itemsWithCellIdentifier("SuggestCell",
+            cellType: UITableViewCell.self)) { row, suggest, cell in
+              cell.textLabel?.attributedText = suggest
             }.addDisposableTo(disposeBag)
+  }
+
+  private func highlightSearchTerm(searchTerm: String?, inSuggestion suggestion: String) -> NSAttributedString? {
+    guard let searchTerm = searchTerm else {
+      return nil
     }
-    
-    private func highlightSearchTerm(searchTerm: String?, inSuggestion suggestion: String) -> NSAttributedString? {
-        guard let searchTerm = searchTerm else {
-            return nil
-        }
-        
-        let searchTermRange = (suggestion as NSString).rangeOfString(searchTerm, options: [.CaseInsensitiveSearch])
-        let attributedString = NSMutableAttributedString(string: suggestion)
-        
-        if searchTermRange.length > 0 {
-            attributedString.addAttributes([NSForegroundColorAttributeName : UIColor.grayColor()], range: searchTermRange)
-        }
-        
-        return NSAttributedString(attributedString: attributedString)
+
+    let searchTermRange = (suggestion as NSString).rangeOfString(searchTerm, options: [.CaseInsensitiveSearch])
+    let attributedString = NSMutableAttributedString(string: suggestion)
+
+    if searchTermRange.length > 0 {
+      attributedString.addAttributes([NSForegroundColorAttributeName : UIColor.grayColor()], range: searchTermRange)
     }
+
+    return NSAttributedString(attributedString: attributedString)
+  }
 
 }
-
